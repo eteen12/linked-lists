5e8258a9f03fdfe01fd71ce2f393e86e03376ede
eteen <eteensmash@gmail.com>
2024-05-15 21:42:38 -0700

    wrap up, learned how to add assistant ai

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 /* eslint-disable */
 class node{
     constructor(data){
-        this.data 
+        this.data = data;
+        this.next = null;
     }
 }
 
+class LinkedList {
+    constructor(){
+        this.head = null;
+        this.length = 0;
+    }
+} 
 
+const list = new LinkedList();
+console.log(list)
+console.log('hey')
 /* eslint-enable */
